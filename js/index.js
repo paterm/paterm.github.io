a61a74d5c160e68a182300830f3f96b7c01f47a2
Efimov <paterm@yandex.ru>
2018-10-17 18:17:40 +0500

    add description strings by typed.js

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+setTimeout(() => {
+	const typed = new Typed('.b-logo__text', {
+		strings: [
+			"Full cycle of development",
+			"Project managment",
+			"Design",
+			"Adaptive imposition",
+			"Frontend",
+			"Backend",
+			"Optimization",
+			"Tests",
+			"&lt;PATERM/&gt;"
+		],
+		typeSpeed: 90
+	});
+}, 2000);
